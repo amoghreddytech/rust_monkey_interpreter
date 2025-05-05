64940d339e783ab17040dd619269d9acd47b5209
Amogh Reddy <amogh.reddy1@gmail.com>
2025-05-05 14:24:38 -0400

    added the module infix_expression

--- src/ast/expressions/mod.rs
+++ src/ast/expressions/mod.rs
@@ -1,7 +1,9 @@
 pub mod identifier;
+pub mod infix;
 pub mod integer;
 pub mod prefix;
 
 pub use identifier::IdentifierExpression;
+pub use infix::InfixExpression;
 pub use integer::IntegerExpression;
 pub use prefix::PrefixExpression;
