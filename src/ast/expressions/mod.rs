2ea3a4fdbc0f38846f97ccd47b9411dab18e40b0
Amogh Reddy <amogh.reddy1@gmail.com>
2025-05-07 13:24:26 -0400

    import the modules

--- src/ast/expressions/mod.rs
+++ src/ast/expressions/mod.rs
@@ -1,8 +1,10 @@
+pub mod boolean;
 pub mod identifier;
 pub mod infix;
 pub mod integer;
 pub mod prefix;
 
+pub use boolean::BooleanExpression;
 pub use identifier::IdentifierExpression;
 pub use infix::InfixExpression;
 pub use integer::IntegerExpression;
