40648fa5ecd187e67a1849f1969cae3212117efb
Amogh Reddy <amogh.reddy1@gmail.com>
2025-05-23 09:05:25 -0400

    created many new things here I can't write a good commit message

--- src/evaluator/evaluator.rs
+++ src/evaluator/evaluator.rs
@@ -1,5 +1,3 @@
-use lazy_static::lazy_static;
-
 use crate::{
     ast::{
         ast::Program,
@@ -11,7 +9,7 @@ use crate::{
         statements::{BlockStatement, ExpressionStatement, LetStatement, ReturnStatement},
         traits::{Node, Statement},
     },
-    object::{Boolean, Integer, Null, Object},
+    object::{Boolean, Integer, Null, Object, Return},
 };
 
 fn eval_bang_operator_expression(right: Box<dyn Object>) -> Box<dyn Object> {
@@ -51,17 +49,17 @@ fn eval_prefix_expression(operator: String, right: Box<dyn Object>) -> Option<Bo
 }
 
 fn eval_integer_infix(operator: String, left: i64, right: i64) -> Box<dyn Object> {
-    if operator == "+" {
-        return Box::new(Integer::new(left + right));
-    } else if operator == "-" {
-        return Box::new(Integer::new(left - right));
-    } else if operator == "/" {
-        return Box::new(Integer::new(left / right));
-    } else if operator == "*" {
-        return Box::new(Integer::new(left * right));
-    } else {
-        Box::new(Null {})
-    }
+    return match operator.as_str() {
+        "+" => Box::new(Integer::new(left + right)),
+        "-" => Box::new(Integer::new(left - right)),
+        "*" => Box::new(Integer::new(left * right)),
+        "/" => Box::new(Integer::new(left / right)),
+        "<" => Box::new(Boolean::new(left < right)),
+        ">" => Box::new(Boolean::new(left > right)),
+        "==" => Box::new(Boolean::new(left == right)),
+        "!=" => Box::new(Boolean::new(left != right)),
+        _ => Box::new(Null {}),
+    };
 }
 
 fn eval_infix_expression(
@@ -78,14 +76,100 @@ fn eval_infix_expression(
             left_int.value,
             right_int.value,
         ));
-    } else {
-        return None;
     }
+    if let (Some(left_bool), Some(right_bool)) = (
+        left.as_any().downcast_ref::<Boolean>(),
+        right.as_any().downcast_ref::<Boolean>(),
+    ) {
+        if operator == "==" {
+            return Some(Box::new(Boolean::new(left_bool.value == right_bool.value)));
+        } else if operator == "!=" {
+            return Some(Box::new(Boolean::new(left_bool.value != right_bool.value)));
+        }
+    }
+
+    return Some(Box::new(Null {}));
+}
+
+fn is_truthy(condition: Box<dyn Object>) -> bool {
+    if let Some(cond) = condition.as_any().downcast_ref::<Null>() {
+        return false;
+    }
+
+    if let Some(cond) = condition.as_any().downcast_ref::<Boolean>() {
+        if cond.value == true {
+            return true;
+        } else if cond.value == false {
+            return false;
+        }
+    }
+
+    true
+}
+
+fn eval_if_expression(node: &ConditionalExpression) -> Box<dyn Object> {
+    let condition = evaluate(node.condition.as_node());
+    if let Some(cond) = condition {
+        if is_truthy(cond) {
+            return evaluate(&node.consequence).expect("this might need to be fixed in the future");
+        } else if node.alternative.is_none() {
+            match &node.alternative {
+                Some(n) => return evaluate(n).expect("this should not fail"),
+                None => return Box::new(Null {}),
+            }
+        }
+    }
+
+    return Box::new(Null {});
+}
+
+fn eval_program(program: &Program) -> Box<dyn Object> {
+    let mut result = Box::new(Null::new()) as Box<dyn Object>;
+
+    for stmt in &program.statements {
+        result = evaluate(stmt.as_node()).unwrap_or_else(|| Box::new(Null::new()));
+
+        if let Some(return_obj) = result.as_any().downcast_ref::<Return>() {
+            return return_obj.value.clone();
+        }
+    }
+
+    result
+}
+
+fn eval_block_statement(block: &BlockStatement) -> Box<dyn Object> {
+    let mut result = Box::new(Null::new()) as Box<dyn Object>;
+    for stmt in &block.statements {
+        result = evaluate(stmt.as_node()).unwrap_or_else(|| Box::new(Null::new()));
+        if let Some(return_obj) = result.as_any().downcast_ref::<Return>() {
+            return result;
+        }
+    }
+
+    result
 }
 
 pub fn evaluate(node: &dyn Node) -> Option<Box<dyn Object>> {
+    if let Some(return_node) = node.as_any().downcast_ref::<ReturnStatement>() {
+        let evaluated_value = return_node
+            .return_value
+            .as_ref()
+            .and_then(|expr| evaluate(expr.as_ref().as_node()))
+            .unwrap_or_else(|| Box::new(Null::new()));
+
+        return Some(Box::new(Return::new(evaluated_value)));
+    }
+
     if let Some(program_node) = node.as_any().downcast_ref::<Program>() {
-        return Some(eval_statements(&program_node.statements));
+        return Some(eval_program(program_node));
+    }
+
+    if let Some(block_statment) = node.as_any().downcast_ref::<BlockStatement>() {
+        return Some(eval_block_statement(block_statment));
+    }
+
+    if let Some(conditional_node) = node.as_any().downcast_ref::<ConditionalExpression>() {
+        return Some(eval_if_expression(conditional_node));
     }
 
     if let Some(prefix_node) = node.as_any().downcast_ref::<PrefixExpression>() {
@@ -166,38 +250,22 @@ pub fn evaluate(node: &dyn Node) -> Option<Box<dyn Object>> {
 }
 
 fn eval_statements<'a>(stmts: &Vec<Box<dyn Statement>>) -> Box<dyn Object> {
-    let mut result: Option<Box<dyn Object>> = None;
+    let mut result: Box<dyn Object> = Box::new(Null {});
 
     for stmt in stmts {
-        let mut node: &dyn Node;
+        result = evaluate(stmt.as_node()).unwrap_or_else(|| Box::new(Null::new()));
 
-        if let Some(let_node) = stmt.as_any().downcast_ref::<LetStatement>() {
-            node = let_node;
-            result = evaluate(node);
-        }
-        if let Some(return_node) = stmt.as_any().downcast_ref::<ReturnStatement>() {
-            node = return_node;
-            result = evaluate(node);
-        }
-        if let Some(block_node) = stmt.as_any().downcast_ref::<BlockStatement>() {
-            node = block_node;
-            result = evaluate(node);
-        }
-        if let Some(expression_node) = stmt.as_any().downcast_ref::<ExpressionStatement>() {
-            node = expression_node;
-            result = evaluate(node);
+        if let Some(return_obj) = result.as_any().downcast_ref::<Return>() {
+            return return_obj.value.clone();
         }
     }
-
-    match result {
-        Some(value) => return value,
-        None => Box::new(Null {}),
-    }
+    result
 }
 
 #[cfg(test)]
 
 mod test {
+
     use crate::{lexer::lexer::Lexer, object::boolean::Boolean, parser::parser::Parser};
 
     use super::*;
@@ -207,12 +275,10 @@ mod test {
         let parser = Parser::new(lexer);
         let mut p = Program::new(parser);
         p.parse_program();
-
         return evaluate(&p);
     }
 
     fn test_integer_object(object: Box<dyn Object>, expected: i64) {
-        println!("{:?}", object);
         let int_object = object
             .as_any()
             .downcast_ref::<Integer>()
@@ -257,8 +323,16 @@ mod test {
 
     #[test]
     fn test_eval_boolean_expression() {
-        let tests: Vec<(String, bool)> =
-            vec![("true;".to_string(), true), ("false".to_string(), false)];
+        let tests: Vec<(String, bool)> = vec![
+            ("true;".to_string(), true),
+            ("false".to_string(), false),
+            ("true == false".to_string(), false),
+            ("true != false".to_string(), true),
+            ("(1 < 2) == true".to_string(), true),
+            ("(1 < 2) == false".to_string(), false),
+            ("(1 > 2) == true".to_string(), false),
+            ("(1 > 2) == false".to_string(), true),
+        ];
 
         for t in tests {
             let (input, output) = t;
@@ -282,6 +356,76 @@ mod test {
             let (input, output) = t;
             let evaluated = test_eval(input);
             test_boolean_object(evaluated.unwrap(), output);
+        }
+    }
+
+    #[test]
+    fn test_if_else_staement() {
+        let tests: Vec<(String, Box<dyn Object>)> = vec![
+            ("if (true) {10};".to_string(), Box::new(Integer::new(10))),
+            ("if (false) {10};".to_string(), Box::new(Null {})),
+            ("if (1) {10};".to_string(), Box::new(Integer::new(10))),
+            ("if (1 < 2) {10};".to_string(), Box::new(Integer::new(10))),
+            ("if (1 > 2) {10};".to_string(), Box::new(Null {})),
+            (
+                "if (1 > 2) {10} else { 20 };".to_string(),
+                Box::new(Integer::new(20)),
+            ),
+            (
+                "if (1 < 2) {10} else { 20 };".to_string(),
+                Box::new(Integer::new(10)),
+            ),
+        ];
+
+        for t in tests {
+            let (input, output) = t;
+            let evaluated = test_eval(input).unwrap();
+            match evaluated.as_any() {
+                e if e.is::<Null>() => {
+                    let x = e.downcast_ref::<Null>().expect("should become null");
+                }
+                e if e.is::<Integer>() => e
+                    .downcast_ref::<Integer>()
+                    .map(|x| {
+                        let o = output.as_any().downcast_ref::<Integer>().expect("Omg");
+                        assert_eq!(x.value, o.value);
+                    })
+                    .expect(""),
+                _ => panic!(),
+            };
+        }
+    }
+
+    fn test_null_object(object: Box<dyn Object>) -> bool {
+        let null_object = object
+            .as_any()
+            .downcast_ref::<Null>()
+            .expect("should be an null object");
+        return true;
+    }
+
+    #[test]
+    fn test_return_objecr() {
+        let tests: Vec<(&str, i64)> = vec![
+            ("return 10;", 10),
+            ("return 10; 9", 10),
+            ("return 2 * 5;", 10),
+            ("9; return 2 * 5; 9;", 10),
+            (
+                "if (10 > 1) {
+                    if (10 > 1) {
+                        return 10;
+                    }
+                    return 1;
+                }",
+                10,
+            ),
+        ];
+
+        for test in tests {
+            let (input, expected) = test;
+            let evaluated = test_eval(input.to_string());
+            test_integer_object(evaluated.unwrap(), expected);
         }
     }
 }
