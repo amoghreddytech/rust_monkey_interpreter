eabcfdced4a59cfb17495e9139ae926cd5573164
Amogh Reddy <amogh.reddy1@gmail.com>
2025-06-18 16:49:25 -0400

    added array and index literals

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -8,11 +8,13 @@ mod statement_structs;
 pub use ast::AbstractSyntaxTree;
 
 pub use expression::Expression;
+pub use expression_structs::array_literal::ArrayLiteral;
 pub use expression_structs::boolean_literal::BooleanLiteral;
 pub use expression_structs::call_literal::CallLiteral;
 pub use expression_structs::function_literal::FunctionLiteral;
 pub use expression_structs::identifier::IdentifierLiteral;
 pub use expression_structs::if_literal::IfLiteral;
+pub use expression_structs::index_expression::IndexLiteral;
 pub use expression_structs::infix_literal::InfixLiteral;
 pub use expression_structs::integer_literal::IntegerLiteral;
 pub use expression_structs::prefix_literal::PrefixLiteral;
